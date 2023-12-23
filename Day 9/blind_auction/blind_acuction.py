87d29f7e286e22053f8e6f2f6670a25f82cc5035
Chris <christianfreud95@gmail.com>
2023-12-23 16:11:34 +0200

    Day 9 changes to auction

--- Day 9/blind_auction/blind_acuction.py
+++ Day 9/blind_auction/blind_acuction.py
@@ -16,20 +16,18 @@ def choose_winner():
     for participant in auction_list:
         if auction_winner[0] == participant["bet"]:
             winner_name = participant["name"]
-    print(len(auction_winner))
     if auction_bets.count(auction_winner[0]) > 1:   # counting if there are more than 1 winner
         auction_winner.clear()  # clear the list and restart
-        print("Two people bet the same amount, restart the auction and try again ")
-
+        print("Two people bet the same amount, the auction will restart \n")
     else:
         print(f"The winner is {winner_name} with a bid of {auction_winner[0]}")
 
 
-add_new_participant("Chis", 1000)
-add_new_participant("Chis2", 3000)
-add_new_participant("Chis3", 3000)
-add_new_participant("Chis4", 5000)
-add_new_participant("Chis5", 5000)
+# add_new_participant("Chis", 1000)
+# add_new_participant("Chis2", 3000)
+# add_new_participant("Chis3", 3000)
+# add_new_participant("Chis4", 5000)
+# add_new_participant("Chis5", 5000)
 
 bidders = "yes"
 while bidders == "yes":     # while loop to add all the bidders to the auction
@@ -39,8 +37,14 @@ while bidders == "yes":     # while loop to add all the bidders to the auction
     add_new_participant(the_name=name, money_amount=bet)    # adding people to auction list, need to redo until all
     # ready
     bidders = input("Are there any other bidders? Type 'yes' or 'no'. \n")
+    choose_winner()
+    if len(auction_winner) == 0:
+        bidders = "yes"
 
-choose_winner()
+
+
+
+
 
 
 
